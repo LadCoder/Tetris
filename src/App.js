ea7cdc47dff1d8a01f8a622e5d51eef489be1349
LadCoder <hbk.lekkas@gmail.com>
2019-10-20 18:34:38 +1100

    Tetris theme song

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import Tetris from './components/Tetris';
+import ReactHowler from 'react-howler';
+import bgMusic from './sounds/tetris.mp3';
 
 const App = () => (
   <div className="App">
+    <ReactHowler
+      src={bgMusic}
+      playing={true}
+      loop={true}
+    />
     <Tetris />
+    
   </div>
 );
 
